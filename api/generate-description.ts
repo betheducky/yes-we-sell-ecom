48ef488f60892f5fe9ef4e6f7d1975a9a97913de
Aren Bruce <devtodaydevtomorrow@gmail.com>
2025-10-28 20:12:59 -0400

    removed comments, ignore type checking for api files

--- api/generate-description.ts
+++ api/generate-description.ts
@@ -1,9 +1,8 @@
-// import OpenAI from "openai";
+// @ts-ignore
 
-import { VercelRequest, VercelResponse } from "@vercel/node";
 const OpenAI = require('openai');
 
-module.exports = async (req: VercelRequest, res: VercelResponse) => {
+module.exports = async (req: any, res: any) => {
     try {
 
         const client = new OpenAI({
@@ -31,30 +30,3 @@ module.exports = async (req: VercelRequest, res: VercelResponse) => {
         return res.status(500).json({ error: err.message || "Server error" })
     }
 }
-
-// const client = new OpenAI({
-//     apiKey: process.env['YES_WE_SELL_API_KEY']
-// });
-
-// export default async function handler(req: VercelRequest, res: VercelResponse) {
-//     try {
-//         const {productTitle} = req.body;
-
-//         if(!productTitle)
-//             return res.status(400).json({ error: 'Missing productTitle' });
-
-//         const completion = await client.chat.completions.create({
-//             model: 'gpt-4o-mini',
-//             messages: [{
-//                 role: 'user',
-//                 content: `Write a short, engaging, e-commerce description (1 sentence long) for "${productTitle}". Include 1 point of value and a likely use case delivered with a salesy tone.`,
-//                 },
-//             ],
-//         });
-
-//         const description = completion.choices[0].message.content;
-//         return res.status(200).json({ description });
-//     } catch(err: any) {
-//         return res.status(500).json({ error: err.message })
-//     }
-// }
--- api/generate-image.ts
+++ api/generate-image.ts
@@ -1,9 +1,8 @@
-// import OpenAI from 'openai';
-import {VercelRequest, VercelResponse} from '@vercel/node';
+// @ts-ignore
 
 const OpenAI = require("openai");
 
-module.exports = async (req: VercelRequest, res: VercelResponse) => {
+module.exports = async (req: any, res: any) => {
     try {
 
         const client = new OpenAI({apiKey: process.env['YES_WE_SELL_API_KEY']});
@@ -25,25 +24,3 @@ module.exports = async (req: VercelRequest, res: VercelResponse) => {
         return res.status(500).json({ error: err.message || "Server error." });
     }
 }
-
-// const client = new OpenAI({apiKey: process.env['YES_WE_SELL_API_KEY']});
-
-// export default async function handler(req: VercelRequest, res: VercelResponse) {
-    // try {
-    //     const {productTitle} = req.body;
-
-    //     if (!productTitle) 
-    //         return res.status(400).json({ error: "Missing productName" });
-
-    //     const image = await client.images.generate({
-    //         model: 'dall-e-3',
-    //         prompt: `Professional e-commerce photo of ${productTitle} on a white background with soft shadows and realistic lighting.`,
-    //         size: '512x512',
-    //     });
-
-    //     return res.status(200).json({ imageUrl: image.data![0].url });
-    // } catch (err: any) {
-    //     console.log("Error generating image:", err);
-    //     return res.status(500).json({ error: err.message || "Server error." });
-    // }
-// }
